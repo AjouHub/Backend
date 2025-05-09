cc306bdf9846b183a0e280c506053f2232c11483
origami0352 <kyn008116@gamil.com>
2025-05-09 15:12:01 +0900

    first commit

---
+++ src/main/java/sulhoe/ajouhub/AjouHubApplication.java
@@ -0,0 +1,13 @@
+package sulhoe.ajouhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AjouHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AjouHubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sulhoe/ajouhub/AjouHubApplicationTests.java
@@ -0,0 +1,13 @@
+package sulhoe.ajouhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AjouHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
