d66fbffb6de1cb4d5cdc4d7cff0b7e7744a6f7da
origami0352 <kyn008116@gamil.com>
2025-07-07 18:21:59 +0900

    feat: entity -> dto 로직 추가

--- src/main/java/sulhoe/ajouhub/dto/notice/NoticeDto.java
+++ src/main/java/sulhoe/ajouhub/dto/notice/NoticeDto.java
@@ -1,5 +1,9 @@
 package sulhoe.ajouhub.dto.notice;
 
+import sulhoe.ajouhub.entity.Notice;
+
+import java.util.List;
+
 public record NoticeDto(String id,
                         String number,
                         String category,
@@ -7,4 +11,23 @@ public record NoticeDto(String id,
                         String department,
                         String date,
                         String link) {
+    public static NoticeDto fromEntity(Notice notice) {
+        return new NoticeDto(
+                notice.getId().toString(),
+                notice.getNumber(),
+                notice.getCategory(),
+                notice.getTitle(),
+                notice.getDepartment(),
+                notice.getDate(),
+                notice.getLink()
+        );
+    }
+
+    public static List<NoticeDto> toDtoList(List<Notice> notices) {
+        return notices.stream()
+                .map(NoticeDto::fromEntity)
+                .toList();
+    }
+
 }
+
